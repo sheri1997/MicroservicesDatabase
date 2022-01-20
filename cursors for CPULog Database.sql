ed26c42fbde2b5d7af578643ef5b918522286ff0
Shreesh Pandey <shvspandey@gmail.com>
2022-01-20 16:21:15 +0530

    Static coursor

--- cursors for CPULog Database.sql
+++ cursors for CPULog Database.sql
@@ -1,14 +1,20 @@
 use MicroservicesDatabase;
 select * from CPULogDatabse;
-declare cursor_cpuworkingtime cursor for
-select user_name, technology from CPULogDatabse where Cpu_Working_Time < 2000
-open cursor_cpuworkingtime
-fetch next from cursor_cpuworkingtime into @username, @tech
-while(@@FETCH_STATUS = 0)
-begin
-	print 'name = '+ cast(@username as nvarchar(50) + 'technology' + @tech
-	fetch next from cursor_cpuworkingtime into @username, @tech
-end
-close cursor_cpuworkingtime
-deallocate cursor_cpuworkingtime
-
+declare @username nvarchar(100)
+declare @tech nvarchar(100)
+declare cur_emp cursor
+static for 
+select user_name, technology from CPULogDatabse
+open cur_emp
+if @@cursor_rows >0
+	begin
+	fetch next from cur_emp into @username, @tech
+	while @@FETCH_STATUS = 0
+		begin
+		print 'Username = ' + @username + 'Technolgy used = ' + @tech
+		fetch next from cur_emp into @username, @tech
+		end
+	end
+close cur_emp
+deallocate cur_emp
+set nocount off
